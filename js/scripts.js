0dea532628b3d61c9f0aa1cfe4287e26b295acf8
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-04 17:37:28 -0800

    practicing calculations with parenthesis

--- js/scripts.js
+++ js/scripts.js
@@ -57,3 +57,8 @@ document.write(simpleAddition);
 let size = 100;
 let doubleSize = size * 2; 
 document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
