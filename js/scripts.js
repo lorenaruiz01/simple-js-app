05a96d5fbd2b2aabd2ccfabb9ce25d0bb86c71d7
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-04 17:27:48 -0800

    commenting the exercise flow

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,39 @@
 // alert('Hello world');//
+
+// INITIAL CODE (EXERCISE 1.1)
 // let myName = 'Bob';
 // document.write(myName);
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
 
- 
+
+// TESTING OUT CODE IN REVERSE
+// document.write(myName);
+// let myName = 'Bob';
+
+// TESTING OUT TWO COMMANDS WITH VALUE REPLACED*
+// let myName = 'Bob';
+// document.write(myName);
+// myName = 'John Doe';
+// document.write(myName);
+//  * THIS CODE WILL WRITE 'BobJohn Doe' IN HTML PAGE 
+
+
+            // SYNTAX - SEMICOLONS
+// NOT RECOMMENDED
+// let myName = 'Anne'
+// alert(myName)
+
+// RECOMMENDED
+// let myName = 'Anne';
+// alert(myName);
+
+
+            // SYNTAX - WRITE EACH COMMAND ON A SEPARATE LINE
+// NOT RECOMMENDED
+// let myName = 'Fred';alert(myName);
+
+// RECOMMENDED
+// let myName = 'Fred';
+// alert(myName);
+
+
+            
