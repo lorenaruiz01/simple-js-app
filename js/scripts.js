b9ca425d958817e12bdfc87e4ac8eb9e5cb1efd3
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-05 13:58:22 -0800

    test out block-scoped variable

--- js/scripts.js
+++ js/scripts.js
@@ -56,14 +56,14 @@
         // let simpleAddition = 2 + 2;
         // document.write(simpleAddition);
 
-        // let size = 100;
-        // let doubleSize = size * 2; 
-        // document.write(doubleSize);
+        let size = 100;
+        let doubleSize = size * 2; 
+        document.write(doubleSize);
 
-        // let size = 100;
-        // let doubleSize = size * 2;
-        // let minSize = (doubleSize * 2) - (size / 2);
-        // document.write(minSize);
+        let size = 100;
+        let doubleSize = size * 2;
+        let minSize = (doubleSize * 2) - (size / 2);
+        document.write(minSize);
 
 alert('Hello world');
 
