9b52d9df05e568e28997d15515e098f398b5cda3
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-05 06:09:40 -0800

    reorganize comment indentations

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,71 @@
 // alert('Hello world');//
 
 // INITIAL CODE (EXERCISE 1.1)
-// let myName = 'Bob';
-// document.write(myName);
+        // let myName = 'Bob';
+        // document.write(myName);
 
 
 // TESTING OUT CODE IN REVERSE
-// document.write(myName);
-// let myName = 'Bob';
+        // document.write(myName);
+        // let myName = 'Bob';
 
 // TESTING OUT TWO COMMANDS WITH VALUE REPLACED*
-// let myName = 'Bob';
-// document.write(myName);
-// myName = 'John Doe';
-// document.write(myName);
-//  * THIS CODE WILL WRITE 'BobJohn Doe' IN HTML PAGE 
+        // let myName = 'Bob';
+        // document.write(myName);
+        // myName = 'John Doe';
+        // document.write(myName);
+        //  * THIS CODE WILL WRITE 'BobJohn Doe' IN HTML PAGE 
 
 
-            // SYNTAX - SEMICOLONS
-// NOT RECOMMENDED
-// let myName = 'Anne'
-// alert(myName)
+// SYNTAX - SEMICOLONS
+        // NOT RECOMMENDED
+        // let myName = 'Anne'
+        // alert(myName)
 
-// RECOMMENDED
-// let myName = 'Anne';
-// alert(myName);
+        // RECOMMENDED
+        // let myName = 'Anne';
+        // alert(myName);
 
 
-            // SYNTAX - WRITE EACH COMMAND ON A SEPARATE LINE
-// NOT RECOMMENDED
-// let myName = 'Fred';alert(myName);
+// SYNTAX - WRITE EACH COMMAND ON A SEPARATE LINE
+        // NOT RECOMMENDED
+        // let myName = 'Fred';alert(myName);
 
-// RECOMMENDED
-// let myName = 'Fred';
-// alert(myName);
+        // RECOMMENDED
+        // let myName = 'Fred';
+        // alert(myName);
 
 
 // COMMENTS
 // There are two types of comments in JavaScript: single-line comments and multi-line comments. You use forward slashes for both. Take a look at some examples below:
 
-javascript
-// Note: We didn’t capitalize the title as this fits our style better
-let title = 'this is the title';
+        // javascript
+        // // Note: We didn’t capitalize the title as this fits our style better
+        // let title = 'this is the title';
 
-/* This is a longer comment.
-It might explain, in detail, why we made a certain decision.
-Or, it could show examples about how to use the following code snippet. */
+        // /* This is a longer comment.
+        // It might explain, in detail, why we made a certain decision.
+        // Or, it could show examples about how to use the following code snippet. */
 
-let title2 = 'Hello!'; // Everything after the slashes is ignored
+        // let title2 = 'Hello!'; // Everything after the slashes is ignored
 
 // *** comments are generally written before the piece of code they reference
 
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
 
-let size = 100;
-let doubleSize = size * 2; 
-document.write(doubleSize);
 
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+// CALCULATION PRACTICE
+
+        // let simpleAddition = 2 + 2;
+        // document.write(simpleAddition);
+
+        // let size = 100;
+        // let doubleSize = size * 2; 
+        // document.write(doubleSize);
+
+        // let size = 100;
+        // let doubleSize = size * 2;
+        // let minSize = (doubleSize * 2) - (size / 2);
+        // document.write(minSize);
+
+
+
