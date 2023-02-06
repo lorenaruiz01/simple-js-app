d74dd06e2a9c1f4a8745f35ee8ac3bc71451563b
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-05 23:13:57 -0800

    create pokemonList array

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let pokemonList = [
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+        { name: 'Pikachu', height: 0.4, types: 'electric'},
+        {name:'Jynx', height: 1.4, types: ['electric', 'ice']}, 
+        {name: 'Jigglypuff', height: 0.5, types:['fairy', 'normal'] }
+];
