9365e570f7cb0eba2db70d28c1d18676a21a4b09
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-11 08:53:43 -0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 let pokemonList = [
-        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-        { name: 'Pikachu', height: 0.4, types: 'electric'},
+        {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+        {name: 'Pikachu', height: 0.4, types: 'electric'},
         {name:'Jynx', height: 1.4, types: ['electric', 'ice']}, 
         {name: 'Jigglypuff', height: 0.5, types:['fairy', 'normal'] }
 ];
@@ -8,3 +8,4 @@ let pokemonList = [
 for (let i = 0; i < pokemonList.length; i++){
         document.write(name + " " + height + "m")
 }
+
