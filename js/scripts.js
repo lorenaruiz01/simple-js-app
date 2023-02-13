9fa9051eb8f60862bda013363dfcb201ea17d31c
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-12 19:03:51 -0800

    edited array syntax

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,13 @@
 let pokemonList = [
-        {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-        {name: 'Pikachu', height: 0.4, types: 'electric'},
-        {name:'Sylveon', height: 3.03, types: 'fairy'}, 
-        {name: 'Jigglypuff', height: 0.5, types:['fairy', 'normal']}
+        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+        { name: 'Pikachu', height: 0.4, types: 'electric'},
+        { name:'Sylveon', height: 3.03, types: 'fairy'}, 
+        { name: 'Jigglypuff', height: 0.5, types:['fairy', 'normal']}
 ];
-
-// `for` LOOP
 for (let i = 0; i < pokemonList.length; i++) {
         document.write(name[i]+ " is" + height[i] + "m tall");
-        let height = 1 
-        if (height > 1 ) {
+        let height = x 
+        if (x > 1 ) {
                 document.write('That is a big pokemon!');
         }
 }
