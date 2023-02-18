38eff1c7e58ea90e5fe745b5d062a51822f5a229
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-17 21:15:45 -0800

    edit if condition

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,6 @@ for (let i = 0; i < pokemonList.length; i++) {
        const pokemon = pokemonList[i]
         document.write("<p>", pokemon.name + " is " + pokemon.height + "m tall" , "</p>");
        if (pokemon.height[i] > 1) {
-                document.write('That is a big pokemon!')
+                document.write("That is a big pokemon!")
         };
 }
