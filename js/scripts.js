6db58e9445f67491411fea6aab65866b78797eb2
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-20 11:22:09 -0800

    edit pokemon.height to remove array code

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,9 @@ let pokemonList = [
 for (let i = 0; i < pokemonList.length; i++) {
         const pokemon = pokemonList[i]
          document.write("<p>", pokemon.name + " is " + pokemon.height + "m tall" , "</p>");
-        if (pokemon.height[i] > 1) {
+        if (pokemon.height > 1) {
                  document.write("That is a big pokemon!")
          };
  }
  
+
