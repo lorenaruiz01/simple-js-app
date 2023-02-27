06ad45e341ed3e67e37496371badd1da8e705786
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-02-26 22:43:47 -0800

    for loop

--- js/scripts.js
+++ js/scripts.js
@@ -13,4 +13,10 @@ for (let i = 0; i < pokemonList.length; i++) {
          };
  }
  
-
+for (let i = 0; i < pokemonList.length; i++) {
+        const pokemon = pokemonList[i]
+         document.write("<p>", pokemon.name + " is " + pokemon.height + "m tall" , "</p>");
+        if (pokemon.height > 1) {
+                 document.write("That is a big pokemon!")
+         };
+ }
