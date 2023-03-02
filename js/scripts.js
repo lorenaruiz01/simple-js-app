8a3b6605dd4fb2a2ff181b6b0b0eb90c644cc4c1
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-01 22:44:10 -0800

    removing incorrect code

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,6 @@ let pokemonRepository = (function (){
                 return pokemonList;
         }
         function add (item){
-                //pokemonRepository.add(item);
                 pokemonList.push(item)
                 //not sure if this is the code needed instead
         }
