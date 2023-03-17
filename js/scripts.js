b88e63458d895b624d286dfb7672926ecb9f9a37
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-16 18:43:14 -0700

    add class to button

--- js/scripts.js
+++ js/scripts.js
@@ -45,6 +45,7 @@ pokemonList.forEach(function(pokemon){
         let listItem = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
+        button.classList.add("button-class");
         listItem.appendChild(button);
         pokemonListItem.appendChild(listItem);
 });
