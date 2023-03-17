7fbd1ee435ed46a5cd14922c8489da73d4d7aac2
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-03-16 18:14:44 -0700

    remove unused function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,3 @@
-(function (){
-let pokemonList = [
-        { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-        { name: 'Pikachu', height: 0.4, types: 'electric'},
-        { name:'Sylveon', height: 3.03, types: 'fairy'}, 
-        { name: 'Jigglypuff', height: 0.5, types:['fairy', 'normal']}
-];
-
-})()
-
 let pokemonRepository = (function (){
         let pokemonList = [
                 {
