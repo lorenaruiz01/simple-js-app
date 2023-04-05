a782d507dfeaf45e25d30bfde4fbcaa463271bc4
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-05 16:51:27 -0700

    add loadlist function

--- js/scripts.js
+++ js/scripts.js
@@ -62,4 +62,21 @@ repository.getAll().forEach(function(pokemon){
 let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-  
+
+  // Other functions remain here
+
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
