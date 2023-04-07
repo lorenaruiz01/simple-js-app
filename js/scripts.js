ec7a5d9c5b93b3cabafb1f467415863810db2e0b
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-07 08:44:48 -0700

    load items code

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ let pokemonRepository = (function () {
         let pokemonList = [];
         let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+
         function getAll(){
                 return pokemonList;
         }
@@ -20,63 +21,64 @@ let pokemonRepository = (function () {
                         showDetails(pokemon);
                       });
         }
-        function showDetails(pokemon){
-                console.log(pokemon);
-        }
-        return {
+        function loadList() {
+                return fetch(apiUrl).then(function (response) {
+                  return response.json();
+                }).then(function (json) {
+                  json.results.forEach(function (item) {
+                    let pokemon = {
+                      name: item.name,
+                      detailsUrl: item.url
+                    };
+                    add(pokemon);
+                  });
+                }).catch(function (e) {
+                  console.error(e);
+                })
+              }
+
+              function loadDetails(item) {
+                let url = item.detailsUrl;
+                return fetch(url).then(function (response) {
+                  return response.json();
+                }).then(function (details) {
+                  // Now we add the details to the item
+                  item.imageUrl = details.sprites.front_default;
+                  item.height = details.height;
+                  item.types = details.types;
+                }).catch(function (e) {
+                  console.error(e);
+                });
+              }
+
+              function showDetails(item){
+                pokemonRepository.loadDetails(item).then(function()
+                {
+                  console.log(item);
+                });
+              }
+
+
+
+
+              return {
+                add: add,
                 getAll: getAll,
-                add:add,
                 addListItem: addListItem,
-                showDetails:showDetails,
-        };
-})();
+                loadList: loadList,
+                loadDetails:loadDetails,
+                showDetails:showDetails
+              };
+        })(); 
 
 
 function myLoopFunction(pokemon) {   
         console.log(pokemon.name + " is " + pokemon.height + "m tall");
 }
-repository.getAll().forEach(function(pokemon){
-        repository.addListItem(pokemon);
-});
-
-
-
-
-
-
-
-
-
-
-  // Other functions remain here
-
-  function loadList() {
-    return fetch(apiUrl).then(function (response) {
-      return response.json();
-    }).then(function (json) {
-      json.results.forEach(function (item) {
-        let pokemon = {
-          name: item.name,
-          detailsUrl: item.url
-        };
-        add(pokemon);
-      });
-    }).catch(function (e) {
-      console.error(e);
-    })
-  }
-
-  return {
-    add: add,
-    getAll: getAll,
-    loadList: loadList
-  };
-})();
-
 
 pokemonRepository.loadList().then(function() {
-  // Now the data is loaded!
-  pokemonRepository.getAll().forEach(function(pokemon){
-    pokemonRepository.addListItem(pokemon);
-  });
-});
+        // Now the data is loaded!
+pokemonRepository.getAll().forEach(function(pokemon){
+        pokemonRepository.addListItem(pokemon);
+        });
+      });
