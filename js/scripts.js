1607835a635427a0c16f10847d3ea1ed84743729
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-04-07 08:17:08 -0700

    comment out pokemonList repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-// let repository = (function (){
-//         let pokemonList = [
-//                 {
-//                         name: 'Bulbasaur',
-//                         height: 0.7,
-//                         types: ['grass', 'poison']
-//                 },
-//                 {
-//                         name: 'Pikachu', 
-//                         height: 0.4, 
-//                         types: 'electric'
-//                 },
-//                 {
-//                         name:'Sylveon', 
-//                         height: 3.03, 
-//                         types: 'fairy'
-//                 },
-//                 {
-//                         name: 'Jigglypuff', 
-//                         height: 0.5, 
-//                         types:['fairy', 'normal']
-//                 }
-//         ]
+let repository = (function (){
+        let pokemonList = [
+                // {
+                //         name: 'Bulbasaur',
+                //         height: 0.7,
+                //         types: ['grass', 'poison']
+                // },
+                // {
+                //         name: 'Pikachu', 
+                //         height: 0.4, 
+                //         types: 'electric'
+                // },
+                // {
+                //         name:'Sylveon', 
+                //         height: 3.03, 
+                //         types: 'fairy'
+                // },
+                // {
+                //         name: 'Jigglypuff', 
+                //         height: 0.5, 
+                //         types:['fairy', 'normal']
+                // }
+        ]
 
         function getAll(){
                 return pokemonList;
