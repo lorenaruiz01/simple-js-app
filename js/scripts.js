85c605c822d3125a5b114b6ea93064fae76b30f8
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-08 15:26:43 -0700

    add list group item class to li

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,7 @@ let pokemonRepository = (function () {
         function addListItem(pokemon){
                 let pokemonListItem = document.querySelector(".pokemon-list");
                 let listItem = document.createElement("li");
+                listItem.classList.add("list-group-item");
                 let button = document.createElement("button");
                 button.innerText = pokemon.name;
                 button.classList.add("button-class");
