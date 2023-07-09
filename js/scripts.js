5536c49b2448a59a607ff1a14e04148b06f1a701
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-07-08 22:29:45 -0700

    remove button classes

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,6 @@ let pokemonRepository = (function () {
                 listItem.classList.add("list-group-item");
                 let button = document.createElement("button");
                 button.innerText = pokemon.name;
-                button.classList.add("button-class btn btn-primary");
                 listItem.appendChild(button);
                 pokemonListItem.appendChild(listItem);
                 button.addEventListener('click', function() {
