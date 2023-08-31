241687968e6a8526353d96b0c37f8ccecc852527
lorenaruiz01 <101694059+lorenaruiz01@users.noreply.github.com>
2023-08-30 20:06:59 -0700

    add smooth scrolling behavior

--- js/scripts.js
+++ js/scripts.js
@@ -178,6 +178,8 @@ function scrollFunction(){                                                // Whe
 }
 
 function backToTopFunction() {                                            // When the user clicks on the button, scroll to the top of the document
-    document.body.scrollTop = 0;                                          // For Safari
-    document.documentElement.scrollTop = 0;                               // For Chrome, Firefox, IE and Opera
+    window.scrollTo({top: 0, behavior: 'smooth'});
+
+    //document.body.scrollTop = 0;                                          // For Safari
+    //document.documentElement.scrollTop = 0;                               // For Chrome, Firefox, IE and Opera
 }
